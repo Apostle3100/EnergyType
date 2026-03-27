5c7f6ddd4ec09fff024978f18195ade4d11c1fd6
apostle3100 <sarfojohnsarfoapostle@gmail.com>
2026-03-27 15:10:55 +0200

    add:work

--- src/model/Bus.java
+++ src/model/Bus.java
@@ -6,12 +6,47 @@ public class Bus extends Vehicle{
 	private int hasBaggageDivision;
 	
 	
+	public int getnumberOfSeats() {
+		return numberOfSeats;
+	}
 	
 	
+	public int gethasBaggageDivision() {
+		return hasBaggageDivision;
+	}
 	
+	public void setnumberOfSeats(int inputnumberOfSeats) {
+		if (inputnumberOfSeats > 5 && inputnumberOfSeats < 100){
+			numberOfSeats = inputnumberOfSeats;
+		}
+		else {
+			numberOfSeats = 5;
+		}
+	}
+
+	
+	public void sethasBaggageDivision(int inputHasBaggageDivision) {
+		hasBaggageDivision = inputHasBaggageDivision;
+		
+		
+	}
+	public Bus() {
+		super();
+		setTitle("opel");
+		setVehicleCode();
+		setnumberOfSeats(10);
+		sethasBaggageDivision(2);
+		
+		
+	}
 	
 		
-
+public Bus (String inputtitle, float inputPrice, EnergyType inputEType, int inputNumberOfSeats,
+		int inputBaggageDivision) {
+	super (inputtitle, inputPrice, inputEType);
+	setnumberOfSeats(inputNumberOfSeats);
+	sethasBaggageDivision(inputBaggageDivision);
+}
 	}
 
 
